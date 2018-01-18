b89ada13960c65cfa2d34cb9fa3f63627c599818
Plamen Jetchev <plamen.bodywork@gmail.com>
2018-01-18 13:47:47 -0500

    Customized show action for portfolio module

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
+# Custom routes
+
 Rails.application.routes.draw do
-  resources :my_portfolios
+  resources :my_portfolios, except: [:show]
+  get 'portfolio/:id', to: 'my_portfolios#show', as: 'portfolio_show'
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
