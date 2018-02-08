62260b15a2a0407803a7692ea975f0eca67d44d2
Plamen Jetchev <pjetchev@gmail.com>
2018-02-08 12:15:55 -0500

    Installed jquery ui, sortable html 5 and css calls

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //= require jquery
-//= require jquery_ujs
 //= require bootstrap-sprockets
+//= require jquery_ujs
+//= require jquery-ui
+//= require html.sortable
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
