c545a977287d0d358895429d1df6518ddb52b45d
Plamen Jetchev <pjetchev@gmail.com>
2018-02-13 21:47:00 -0500

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,4 +59,15 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Plamen Jetchev", time: 2500)
+  end
 end
