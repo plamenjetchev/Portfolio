aedf58ccb1853d1195f5f86d6dbd6709e0216087
Plamen Jetchev <pjetchev@gmail.com>
2018-02-15 14:10:23 -0500

    Implemented twitter call on tech-tweets page

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'tech-tweets', to: 'pages#tech_tweets'
 
   resources :blogs do
     member do
