66f4b667ccc4d7d52918a289f4425e4478283510
Plamen Jetchev <pjetchev@gmail.com>
2018-03-08 13:35:54 -0500

    Updating seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
+User.create!(
+  email: "test@test.com",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  name: "Admin",
+  roles: "site_admin"
+)
+
+puts "Admin created"
+
+User.create!(
+  email: "test2@test.com",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  name: "Regular User",
+)
+
+puts "Regular user created"
+
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
